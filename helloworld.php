a5f73bc0c64a4966737e6e8ad57385d4f6b12f6e
Tevin Pawiroredjo <tevin.pawiroredjo@edu-kw1c.nl>
2020-02-04 12:38:51 +0100

    hello world

---
+++ helloworld.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+       <meta charset="utf-8" />
+       <title><?php print "pagina 1"; ?></title>
+    </head>
+    <body><h1>
+        <?php
+        echo "Hallo wereld";
+        ?>
+    </h1>
+
+    </body>
+</html>
---
+++ template.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * User:
+ * Date: 2/3/2018
+ * Time: 12:06 PM
+ * Bestand: template.php
+ * Template voor alle opdrachten
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link type="text/css" rel="stylesheet" href="../css/style.css">
+    <meta charset="utf-8" />
+    <title>titel</title>
+</head>
+<body>
+<header><h1>ICT academie AO</h1></header>
+<div id="wrapper">
+    <h2>Opdracht ?</h2>
+    <p>
+        <?php
+        //Uitwerking opdracht
+        ?>
+    </p>
+    <a href="../index.php">Terug</a>
+</div>
+</body>
+</html>
