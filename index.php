5301bcc8f0fdbbccc61938f777ac42dffce89aac
Tevin Pawiroredjo <tevin.pawiroredjo@edu-kw1c.nl>
2020-02-05 15:23:12 +0100

    Index.php Added "Opdrachten" links Added styling

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * User: Tevin Pawiroredjo
+ * Date: 5/2/2020
+ * Time: 14:58
+ * Bestand: index.php
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <meta charset="utf-8" />
+    <title>PHP Opdrachten</title>
+</head>
+<body>
+<header><h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1></header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 1
+            <ul>
+                <li>
+                    <a href="Hoofdstuk01/helloworld.php">Opdracht 1.1</a>
+                </li>
+                <li>
+                    <a href="Hoofdstuk01/helloworld.php">Opdracht 1.2</a>
+                </li>
+            </ul>
+        </li>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="Hoofdstuk01/helloworld.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="Hoofdstuk01/helloworld.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+        <li>Hoofdstuk 3
+            <ul>
+                <li>
+                    <a href="Hoofdstuk01/helloworld.php">Opdracht 3.1</a>
+                </li>
+            </ul>
+        </li>
+        <li>Hoofdstuk 4
+            <ul>
+                <li>
+                    <a href="Hoofdstuk01/helloworld.php">Opdracht 4.1</a>
+                </li>
+                <li>
+                    <a href="Hoofdstuk01/helloworld.php">Opdracht 4.2</a>
+                </li>
+                <li>
+                    <a href="Hoofdstuk01/helloworld.php">Opdracht 4.3</a>
+                </li>
+                <li>
+                    <a href="Hoofdstuk01/helloworld.php">Opdracht 4.4</a>
+                </li>
+            </ul>
+        </li>
+        <li>Hoofdstuk 5
+            <ul>
+                <li>
+                    <a href="Hoofdstuk01/helloworld.php">Opdracht 5.1</a>
+                </li>
+                <li>
+                    <a href="Hoofdstuk01/helloworld.php">Opdracht 5.2</a>
+                </li>
+                <li>
+                    <a href="Hoofdstuk01/helloworld.php">Opdracht 5.3</a>
+                </li>
+                <li>
+                    <a href="Hoofdstuk01/helloworld.php">Opdracht 5.4</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+</main>
+</body>
+</html>
