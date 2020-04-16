d90eb317c54618ca321b50b45a4b3605648e2527
Tevin Pawiroredjo <tevin.pawiroredjo@edu-kw1c.nl>
2020-04-16 18:34:04 +0200

    Edited form in Opdracht 5.3 Added form_data53.php Added/Edited functions.php in include folder

--- Hoofdstuk05/opdracht5.1.php
+++ Hoofdstuk05/opdracht5.1.php
@@ -12,9 +12,56 @@ include "../includes/header.php";
     <main id="wrapper">
         <h2>Uitwerkingen</h2>
         <p>
-            <?php
+            <form action="form_data.php" method="post">
+                <table>
+                    <tr>
+                        <td>
+                            <label for="bedrijfsnaam">Bedrijfsnaam </label>
+                            <br>
+                            <input type="text" name="bedrijfsnaam" id="bedrijfsnaam">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="voornaam">Voornaam </label>
+                            <br>
+                            <input type="text" name="voornaam" id="voornaam">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="achternaam">Achternaam </label>
+                            <br>
+                            <input type="text" name="achternaam" id="achternaam">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="telefoon">Telefoon </label>
+                            <br>
+                            <input type="text" name="telefoon" id="telefoon">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="email">E-mail </label>
+                            <br>
+                            <input type="text" name="email" id="email">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="bericht">Bericht </label>
+                            <br>
+                            <input type="text" name="bericht" id="bericht">
+                        </td>
+                    </tr>
+                </table>
+                <input type="submit">
 
-            ?>
+            <!-- Geen robot -->
+
+        </form>
         </p>
         <a href="../index.php">Terug</a>
     </main>
