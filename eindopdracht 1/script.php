dbd3c74967509ffbec834725d216e4fdf0d3937e
Tevin Pawiroredjo <tevin.pawiroredjo@edu-kw1c.nl>
2020-04-19 14:21:47 +0200

    Edited folder "eindopdracht 1" including files "login.php","script.php".

--- eindopdracht 1/script.php
+++ eindopdracht 1/script.php
@@ -10,7 +10,7 @@
 include "../includes/header.php";
 
 //10 users
-$user = array("Pieter"=>"Post!","Al"=>"K0h0l");
+$user = array("Pieter"=>"Post!","Al"=>"K0h0l","Guys"=>"Gals","Koekie"=>"Monster","John"=>"W!ck","Hans"=>"Gl0ck","Arthur"=>"M0rgaN","Phil"=>"123123","Bruce"=>"L33","Chuck"=>"N0RR1s");
 $message="";
 
 $x = $_POST["username"];
@@ -21,19 +21,21 @@ $y = $_POST["password"];
 foreach($user as $name => $password) {
     if($name = $x && $password = $y)
     {
-        $message = "Welkom";
-        // Openingstijdstijden
-        // Do: 22:00
-        // Vr: All day
-        // Za: All day
-        // Zo: 12:00
-
-        // Adresgegevens
-        // Am Wriezener Bahnhof
-        // 10243 Berlin
-        // Duitsland
 
         // Deze gegevens dien je ten alle geheim te houden!
+        $message = "<fieldset>
+                        <legend>Openingstijden</legend>
+                        <label>Do: 22:00</label><br>
+                        <label>Vr: All day</label><br>
+                        <label>Za: All day</label><br>
+                        <label>Zo: 12:00</label>
+                    </fieldset>
+                    <fieldset>
+                        <legend>Adresgegevens</legend>
+                        <label>Am Wriezener Bahnhof</label><br>
+                        <label>10243 Berlin</label><br>
+                        <label>Duitsland</label>
+                    </fieldset>";
 
     }
     else
